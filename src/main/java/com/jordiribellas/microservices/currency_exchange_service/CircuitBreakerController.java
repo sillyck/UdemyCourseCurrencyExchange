f2c1d920747aab1ded9a00adba66e870f2c970d5
Jordi Ribellas <jordiribellasramos@gmail.com>
2024-11-21 13:31:04 +0100

    created some dummy url to get a 500 from it

--- src/main/java/com/jordiribellas/microservices/currency_exchange_service/CircuitBreakerController.java
+++ src/main/java/com/jordiribellas/microservices/currency_exchange_service/CircuitBreakerController.java
@@ -1,14 +1,19 @@
 package com.jordiribellas.microservices.currency_exchange_service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
 	@GetMapping("/sample-api")
+	//@Retry
 	public String sampleApi() {
-		return "Sample API";
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-url-to-fail", String.class);
+		
+		return forEntity.getBody();
 	}
 	
 }
